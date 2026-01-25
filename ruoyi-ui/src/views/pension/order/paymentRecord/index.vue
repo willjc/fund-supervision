88c9281c3aa2b8e5353ac479801232464e3f6ab2
willjc <8863704@qq.com>
2026-01-26 01:53:43 +0800

    01260153

--- ruoyi-ui/src/views/pension/order/paymentRecord/index.vue
+++ ruoyi-ui/src/views/pension/order/paymentRecord/index.vue
@@ -65,38 +65,6 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['payment:record:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['payment:record:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['payment:record:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
           type="warning"
           plain
           icon="el-icon-download"
@@ -118,9 +86,7 @@
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="paymentList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="支付ID" align="center" prop="paymentId" width="80"/>
+    <el-table v-loading="loading" :data="paymentList">
       <el-table-column label="支付流水号" align="center" prop="paymentNo" width="180"/>
       <el-table-column label="订单号" align="center" prop="orderNo" width="180"/>
       <el-table-column label="老人姓名" align="center" prop="elderName" width="100"/>
@@ -139,18 +105,13 @@
           <dict-tag :options="dict.type.payment_status" :value="scope.row.paymentStatus"/>
         </template>
       </el-table-column>
-      <el-table-column label="第三方交易号" align="center" prop="thirdPartyTransactionId" width="180" show-overflow-tooltip/>
+      <el-table-column label="第三方交易号" align="center" prop="transactionId" width="180" show-overflow-tooltip/>
       <el-table-column label="支付时间" align="center" prop="paymentTime" width="160">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.paymentTime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="创建时间" align="center" prop="createTime" width="160">
-        <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.createTime) }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="180">
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="100">
         <template slot-scope="scope">
           <el-button
             size="mini"
@@ -158,28 +119,6 @@
             icon="el-icon-view"
             @click="handleDetail(scope.row)"
           >详情</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['payment:record:edit']"
-          >修改</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-refresh"
-            @click="handleUpdateStatus(scope.row)"
-            v-if="scope.row.paymentStatus === '1'"
-            v-hasPermi="['payment:record:status']"
-          >更新状态</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
-            v-hasPermi="['payment:record:remove']"
-          >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -192,60 +131,40 @@
       @pagination="getList"
     />
 
-    <!-- 添加或修改支付记录对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="100px">
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="订单号" prop="orderNo">
-              <el-input v-model="form.orderNo" placeholder="请输入订单号" />
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="支付方式" prop="paymentMethod">
-              <el-select v-model="form.paymentMethod" placeholder="请选择支付方式">
-                <el-option
-                  v-for="dict in dict.type.payment_method"
-                  :key="dict.value"
-                  :label="dict.label"
-                  :value="dict.value"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="支付金额" prop="paymentAmount">
-              <el-input-number v-model="form.paymentAmount" :precision="2" :min="0" style="width: 100%"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="支付状态" prop="paymentStatus">
-              <el-select v-model="form.paymentStatus" placeholder="请选择支付状态">
-                <el-option
-                  v-for="dict in dict.type.payment_status"
-                  :key="dict.value"
-                  :label="dict.label"
-                  :value="dict.value"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-form-item label="第三方交易号" prop="thirdPartyTransactionId">
-          <el-input v-model="form.thirdPartyTransactionId" placeholder="请输入第三方交易号" />
-        </el-form-item>
-        <el-form-item label="网关响应" prop="gatewayResponse">
-          <el-input v-model="form.gatewayResponse" type="textarea" placeholder="请输入网关响应" />
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-      </el-form>
+    <!-- 支付记录详情对话框 -->
+    <el-dialog title="支付记录详情" :visible.sync="detailOpen" width="700px" append-to-body>
+      <el-descriptions :column="2" border>
+        <el-descriptions-item label="支付流水号">{{ detailForm.paymentNo }}</el-descriptions-item>
+        <el-descriptions-item label="订单号">{{ detailForm.orderNo }}</el-descriptions-item>
+        <el-descriptions-item label="老人姓名">{{ detailForm.elderName }}</el-descriptions-item>
+        <el-descriptions-item label="养老机构">{{ detailForm.institutionName }}</el-descriptions-item>
+        <el-descriptions-item label="支付金额">
+          <span style="color: #67C23A; font-weight: bold">¥{{ detailForm.paymentAmount }}</span>
+        </el-descriptions-item>
+        <el-descriptions-item label="支付方式">
+          <dict-tag :options="dict.type.payment_method" :value="detailForm.paymentMethod"/>
+        </el-descriptions-item>
+        <el-descriptions-item label="支付状态">
+          <dict-tag :options="dict.type.payment_status" :value="detailForm.paymentStatus"/>
+        </el-descriptions-item>
+        <el-descriptions-item label="支付时间">{{ parseTime(detailForm.paymentTime) }}</el-descriptions-item>
+        <el-descriptions-item label="第三方交易号" :span="2">{{ detailForm.transactionId || '-' }}</el-descriptions-item>
+        <el-descriptions-item label="网关响应" :span="2">{{ detailForm.gatewayResponse || '-' }}</el-descriptions-item>
+        <el-descriptions-item label="支付凭证" :span="2" v-if="detailForm.paymentProof">
+          <el-image
+            :src="detailForm.paymentProof"
+            :preview-src-list="[detailForm.paymentProof]"
+            fit="cover"
+            style="width: 100px; height: 100px;"
+          />
+        </el-descriptions-item>
+        <el-descriptions-item label="凭证备注" :span="2" v-if="detailForm.paymentProofRemark">{{ detailForm.paymentProofRemark }}</el-descriptions-item>
+        <el-descriptions-item label="创建时间">{{ parseTime(detailForm.createTime) }}</el-descriptions-item>
+        <el-descriptions-item label="操作人">{{ detailForm.operator || '-' }}</el-descriptions-item>
+        <el-descriptions-item label="备注" :span="2">{{ detailForm.remark || '-' }}</el-descriptions-item>
+      </el-descriptions>
       <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
+        <el-button @click="detailOpen = false">关 闭</el-button>
       </div>
     </el-dialog>
 
@@ -256,7 +175,7 @@
 </template>
 
 <script>
-import { listPayment, getPayment, delPayment, addPayment, updatePayment, updatePaymentStatus, exportPayment, getPaymentStatistics } from "@/api/order/paymentRecord";
+import { listPayment, getPayment, exportPayment, getPaymentStatistics } from "@/api/order/paymentRecord";
 import PaymentStatistics from './components/PaymentStatistics'
 
 export default {
@@ -269,12 +188,6 @@ export default {
     return {
       // 遮罩层
       loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
       // 显示搜索条件
       showSearch: true,
       // 总条数
@@ -283,10 +196,10 @@ export default {
       paymentList: [],
       // 日期范围
       dateRange: [],
-      // 弹出层标题
-      title: "",
-      // 是否显示弹出层
-      open: false,
+      // 是否显示详情对话框
+      detailOpen: false,
+      // 详情数据
+      detailForm: {},
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -296,23 +209,6 @@ export default {
         paymentMethod: null,
         paymentStatus: null,
         elderName: null,
-      },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-        orderNo: [
-          { required: true, message: "订单号不能为空", trigger: "blur" }
-        ],
-        paymentAmount: [
-          { required: true, message: "支付金额不能为空", trigger: "blur" }
-        ],
-        paymentMethod: [
-          { required: true, message: "支付方式不能为空", trigger: "change" }
-        ],
-        paymentStatus: [
-          { required: true, message: "支付状态不能为空", trigger: "change" }
-        ]
       }
     };
   },
@@ -330,32 +226,6 @@ export default {
         this.loading = false;
       });
     },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        paymentId: null,
-        paymentNo: null,
-        orderId: null,
-        orderNo: null,
-        paymentAmount: null,
-        paymentMethod: null,
-        paymentStatus: "1",
-        thirdPartyTransactionId: null,
-        gatewayResponse: null,
-        paymentTime: null,
-        createBy: null,
-        createTime: null,
-        updateBy: null,
-        updateTime: null,
-        remark: null
-      };
-      this.resetForm("form");
-    },
     /** 搜索按钮操作 */
     handleQuery() {
       this.queryParams.pageNum = 1;
@@ -367,58 +237,6 @@ export default {
       this.resetForm("queryForm");
       this.handleQuery();
     },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.paymentId)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加支付记录";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const paymentId = row.paymentId || this.ids
-      getPayment(paymentId).then(response => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改支付记录";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.paymentId != null) {
-            updatePayment(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            addPayment(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
-          }
-        }
-      });
-    },
-    /** 删除按钮操作 */
-    handleDelete(row) {
-      const paymentIds = row.paymentId || this.ids;
-      this.$modal.confirm('是否确认删除支付记录编号为"' + paymentIds + '"的数据项？').then(function() {
-        return delPayment(paymentIds);
-      }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("删除成功");
-      }).catch(() => {});
-    },
     /** 导出按钮操作 */
     handleExport() {
       this.download('payment/record/export', {
@@ -427,16 +245,10 @@ export default {
     },
     /** 查看详情 */
     handleDetail(row) {
-      this.$modal.msgInfo(`支付流水号: ${row.paymentNo}, 支付金额: ¥${row.paymentAmount}`);
-    },
-    /** 更新支付状态 */
-    handleUpdateStatus(row) {
-      this.$modal.confirm('确认将支付状态更新为成功吗？').then(() => {
-        return updatePaymentStatus(row.paymentId, '2');
-      }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("状态更新成功");
-      }).catch(() => {});
+      getPayment(row.paymentId).then(response => {
+        this.detailForm = response.data;
+        this.detailOpen = true;
+      });
     },
     /** 支付统计 */
     handleStatistics() {
